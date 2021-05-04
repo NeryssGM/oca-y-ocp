3875899ba1201a12c814849908f9169affba77a2
Hugo Chaparro <hchaparro3@dxc.com>
2021-05-04 10:52:36 -0500

    Proyectos
    Se sube proyecto JAVA

---
+++ OCP/Códigos OCP/001_Advanced_Class_Design/src/pkg001_advanced_class_design/P054_Access_Modifiers.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg001_advanced_class_design;
+
+/**
+ *
+ * @author HUGO
+ */
+public class P054_Access_Modifiers {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
+

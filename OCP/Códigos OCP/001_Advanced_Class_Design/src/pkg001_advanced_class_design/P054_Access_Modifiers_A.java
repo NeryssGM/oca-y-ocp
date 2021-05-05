53224c544579d8c4c96c936b8e9d0fc17d3e156c
Hugo Chaparro <hchaparro3@dxc.com>
2021-05-05 07:18:49 -0500

    Modificación
    Se modifica

--- OCP/Códigos OCP/001_Advanced_Class_Design/src/pkg001_advanced_class_design/P054_Access_Modifiers_A.java
+++ OCP/Códigos OCP/001_Advanced_Class_Design/src/pkg001_advanced_class_design/P054_Access_Modifiers_A.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pkg001_advanced_class_design;
 
 /**
  *
- * @author HUGO
+ * @author HUGO 
  */
 public class P054_Access_Modifiers_A {
     /*

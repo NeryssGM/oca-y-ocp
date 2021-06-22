867891ceb459fb2386a9e56ca7b65275d5530577
Hugo Chaparro <hchaparro3@dxc.com>
2021-06-22 06:52:41 -0500

    Adapter
    Add the Pattern Desing Adapter.

---
+++ OCP/Códigos OCP/999_Pattern Design/src/PD004_Adapter/BandElectric.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PD004_Adapter;
+
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author HUGO
+ */
+public class BandElectric {
+    
+    public static void Play(ArrayList<IInstrumentElectric> instrumentos){
+        instrumentos.forEach((inst)->{
+               inst.TurnOn();
+        });
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(BandElectric.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        instrumentos.forEach((inst)->{
+             inst.TurnOff();
+        });
+    }
+    
+}
---
+++ OCP/Códigos OCP/999_Pattern Design/src/PD004_Adapter/IInstrumentAcoustic.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PD004_Adapter;
+
+/**
+ *
+ * @author HUGO
+ */
+public interface IInstrumentAcoustic {
+    void Play();
+    void Leave();
+}
